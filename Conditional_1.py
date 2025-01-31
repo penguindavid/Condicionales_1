e7ceba880699b748752bd0d50c9ba53422ed2325
penguindavid <davidmacias@colegioguanenta.edu.co>
2025-01-31 08:37:53 -0500

    se agrego con_1

--- Conditional_1.py
+++ Conditional_1.py
@@ -1,2 +1,11 @@
 #input 
 Cant_minutos=input("digite la cant_minuto")
+Cant_minutos=int(Cant_minutos)
+
+#procesing 
+if Cant_minutos<=3:
+    valor_llamada=300
+else:
+    valor_llamada=300+50*(Cant_minutos-3)
+#output
+print("el valor de la llamada es:" + str(valor_llamada))
